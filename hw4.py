34a5a9dc2c3658f042bc3baf5e05fabb0fb445d2
SpencerD1 <spencerwdickinson@gmail.com>
2019-03-28 11:03:18 -0400

    changed name of files

--- hw4.py
+++ hw4.py
@@ -1,68 +1,149 @@
 from __future__ import division
 from __future__ import print_function
 from ortools.sat.python import cp_model
+import csv
+import sys
 
+FIELDS = ['ID','CLASS','CRN','TREE','BRANCH','COURSE_CEILING',
+          'MAJOR','MAJOR2','SUBJ','NUMB','SEQ']
+
+#returns crn of (id, tree, branch)
+def findCrn(id, tree, branch, rowId, rowCrn, rowTree, rowBranch):
+    for row in range(0, len(rowCrn)):
+        if id == rowId[row] and tree == rowTree[row] and branch == rowBranch[row]:
+            return rowCrn[row]
+    return None
+
+
+#returns number of students in each class
+def classSize():
+    classSizes = {}
+    for i in range(0, len(rowCrn) ):
+        if rowCrn[i] not in classSizes:
+            classSizes.append(rowCrn[i])
+        classSizes[rowCrn[i]]+=students[(rowId[i], rowTree[i], rowBranch[i])]
+    return classSizes
 
 def read_file(filename):
-    """Returns data read in from supplied WebTree data file.
-
-    Parameters:
-        filename - string containing the name of the CSV file.
-
-    Returns:
-        a) A dictionary mapping student IDs to records, where each record
-           contains information about that student's WebTree requests.
-        b) A dictionary mapping class years to student IDs, indicating
-           which students are seniors, juniors, etc.
-        c) A dictionary mapping course CRNs to enrollment capacities.
-    """
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=FIELDS)
+
         student_requests = {}
-        students_by_class = {'SENI': set([]), 'JUNI': set([]),
-                             'SOPH': set([]), 'FRST': set([]),
-                             'OTHER': set([])}
-        courses = {}
-        reader.next() # consume the first line, which is just column headers
+        courseCeilings = {}
+        rowId = {}
+        rowTree = {}
+        rowBranch = {}
+        rowCrn = {}
 
+        #reader.next() # consume the first line, which is just column headers
+        skip = True
+        i = 0
         for row in reader:
-            id = int(row['ID'])
-            class_year = row['CLASS']
-            crn = int(row['CRN'])
-            tree = int(row['TREE'])
-            branch = int(row['BRANCH'])
-            if id in student_requests: # does this student already exist?
-                student_requests[id].add_request(crn, tree, branch)
-            else: # nope, create a new record
-                s = Student(id, class_year)
-                s.add_request(crn, tree, branch)
-                student_requests[id] = s
+            if skip == True:
+                skip = False
+            else:
+                id = int(row['ID'])
+                class_year = row['CLASS']
+                crn = int(row['CRN'])
+                tree = int(row['TREE'])
+                branch = int(row['BRANCH'])
 
-            students_by_class[class_year].add(id)
-            courses[crn] = int(row['COURSE_CEILING'])
+                #unique students
+                if id not in student_requests:
+                    student_requests[id] = {}
 
-    return student_requests, students_by_class, courses
+                student_requests[id][tree, branch] = crn
 
+                courseCeilings[crn] = int(row['COURSE_CEILING'])
+
+                #the data based on the row number given on the excel sheet
+                #(helpful for crn-(id,tree,branch) conversion
+                rowId[i]=id
+                rowTree[i] = tree
+                rowBranch[i] = branch
+                rowCrn[i] = int(row['CRN'])
+                i+=1
+    return  student_requests, courseCeilings, rowId, rowTree, rowBranch, rowCrn
 
 def main():
+
+    if (len(sys.argv) != 2):
+        print()
+        print ("***********************************************************")
+        print ("You need to supply a .csv file containing the WebTree data")
+        print ("as a command-line argument.")
+        print()
+        print ("Example:")
+        print ("    python baseline_webtree.py spring-2015.csv")
+        print ("***********************************************************")
+        print()
+        return
+
+    student_requests, courseCeilings, rowId, rowTree, rowBranch, rowCrn = read_file(sys.argv[1])
     model = cp_model.CpModel()
 
-    #CREATE VARIABLES
-    studentClasses = {}
-    for id in ids:
-        for tree in trees:
-            for branch in branches:
-                students[(id, tree, branch)] = model.NewBoolVar('student_id%itree%ibranch%1' % (id, tree, branch))
+    #the tree structure, helpful when assigning random variables
+    #so that you don't add to tree 4 branch 7
+    trees={1:range(1,8), 2:range(1,8), 3:range(1,8), 4:range(1,5)}
 
-    #CREATE CONSTRAINTS
-    for id in ids:
-        model.Add(sum(ids[(id, tree, branch)] for tree in trees for branch in branches) <=4)
-        #model.Add(sum(ids[(id, tree, branch)] for tree in trees for branch in branches) >=3)
-        #SHOULD WE LIMIT?
+    #VARIABLES
+    students = {}
+    for id in student_requests.keys():
+        for tree in trees.keys():
+            for branch in trees[tree]:
+                students[(id, tree, branch)] = model.NewBoolVar('student_id%itree%ibranch%i' % (id, tree, branch))
 
-    for crn in classeSizes:
-        model.Add(classeSizes[crn] <= courseCeilings[crn])
+    #CONSTRAINTS
+        #1. students can have 2-4 classes
+    for id in student_requests.keys():
+        model.Add(sum(students[(id, tree, branch)] for tree in trees.keys() for branch in trees[tree]) <=4)
+        model.Add(sum(students[(id, tree, branch)] for tree in trees.keys() for branch in trees[tree]) >=2)
 
-    for id in ids:
-        for tree in range(0,3): #for tree 1, 2, and 3
-            model.Add(if(ids[id, tree, 0] == False))
+        #2. cannot be assigned to a branch with no classes in it
+    for id in student_requests.keys():
+        for tree in trees.keys():
+            for branch in trees[tree]:
+                if (tree, branch) not in student_requests[id]:
+                    model.Add(students[(id, tree, branch)] == 0)
+
+        #3. class size cannot be bigger than the ceiling
+        classSizes = {}
+        for i in range(0, len(rowCrn) ):
+            if rowCrn[i] not in classSizes:
+                classSizes[rowCrn[i]] = 0
+            classSizes[rowCrn[i]]+=students[(rowId[i], rowTree[i], rowBranch[i])]
+            model.Add(classSizes[rowCrn[i]] <= courseCeilings[rowCrn[i]])
+
+        #4. Cannot have repeats
+
+
+    #THE OBJECTIVE FUNCTION
+        #penalty ideas: scale penalty by reverse year -> penalty of sr = 4*fr
+    model.Minimize(
+    sum(tree * branch * students[(id, tree, branch)] for id in student_requests.keys()
+        for tree in trees for branch in trees[tree]))
+
+    # Creates the solver and solve.
+    solver = cp_model.CpSolver()
+    solver.Solve(model)
+
+    # Statistics.
+    print()
+    print('Statistics')
+    print('  - Unhappiness = %i' % solver.ObjectiveValue())
+    print('  - wall time       : %f s' % solver.WallTime())
+
+    for id in student_requests.keys():
+        print()
+        print('ID: %d' % id)
+        for tree in trees.keys():
+            for branch in trees[tree]:
+                if solver.Value(students[(id, tree, branch)]):
+                    print('(%d, %d) -> %d' % (tree, branch, findCrn(id, tree, branch, rowId, rowCrn, rowTree, rowBranch)))
+
+#can make illegal trees -> quantify how many illegal trees <- for paper
+#for project find flaws, find way to quantify
+    #how many repeat classes people get
+
+if __name__ == '__main__':
+    main()
